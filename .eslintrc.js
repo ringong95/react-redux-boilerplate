3f53727ba370f99cb26f2cc91696594649aa6c97
Ringo Ng <Ringong95@gmail.com>
2019-03-11 14:13:59 -0700

    Updated Eslint file.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
     "extends": "airbnb",
     "parser": "babel-eslint",
     rules:{
-        "linebreak-style": ["error", "windows"]
+        "linebreak-style": ["error", "windows"],
+        "react/react-in-jsx-scope": 0,
+        "no-console": 1
     }
 };
